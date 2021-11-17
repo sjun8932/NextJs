0f2ef88590ae67aeed4fa12362204c6982602984
SANGJUN <sjun8932@gmail.com>
2021-11-17 20:52:29 +0900

    21. 리듀서 쪼개기

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -1,59 +1,19 @@
 import { HYDRATE } from "next-redux-wrapper";
+import user from './user';
+import post from './post';
+import {combineReducers} from "redux";
 
-const initialState = {
-    user: {
-        isLoggedIn: false,
-        user: null,
-        signUpData: {},
-        loginData: {},
+const rootReducer = combineReducers({
+    index: (state = {}, action) => {
+        switch (action.type){
+            case HYDRATE:
+                console.log('HYDRATE' , action);
+                return { ...state, ...action.payload };
+            default :return state;
+        }
     },
-    post: {
-        mainPosts: []
-    }
-}
-
-export const loginAction = (data) => {
-    return {
-        type: 'LOG_IN',
-        data,
-    }
-}
-
-export const logoutAction = (data) => {
-    return {
-        type: 'LOG_OUT',
-        data,
-    }
-}
-
-const rootReducer = (state= initialState,action) => {
-    switch (action.type){
-
-        case HYDRATE:
-            return {
-                ...state, ...action.payload
-            }
-
-        case "LOG_IN":
-            return{
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: true,
-                    user: action.data,
-                }
-            }
-        case "LOG_OUT":
-            return{
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: false,
-                    user: null,
-                }
-            }
-        default :return state;
-    }
-};
+    user,
+    post,
+});
 
 export default rootReducer;
