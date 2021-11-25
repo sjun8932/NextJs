41297b54302d8464c7a9b466e023f842937a6d7d
SANGJUN <sjun8932@gmail.com>
2021-11-25 21:06:33 +0900

    24. 댓글 구현하기

---
+++ prepare/front/components/CommentForm.js
@@ -0,0 +1,27 @@
+import {Button, Form, Input } from 'antd'
+import React, {useCallback} from 'react';
+import useInput from "../hooks/useInput";
+import PropTypes from "prop-types";
+import {useSelector} from "react-redux";
+
+const CommentForm = ({ post }) => {
+    const id = useSelector((state) => state.user.me?.id);
+    const [commentText, onChangeCommentText] = useInput('');
+    const onsubmitComment = useCallback(()=>{
+        console.log(post.id, commentText);
+    },[commentText])
+    return (
+        <Form onFinish={onsubmitComment}>
+            <Form.Item style={{ position: 'relative' , margin: 0 }}>
+                <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4}/>
+            </Form.Item>
+            <Button style={{ position: 'absolute' , right: 0, bottom: -40 }}type="primary" htmlType="submit">삐약</Button>
+        </Form>
+    )
+}
+
+CommentForm.propTypes = {
+    post: PropTypes.object.isRequired,
+}
+
+export default CommentForm;
