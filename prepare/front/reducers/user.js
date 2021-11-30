84a8cdc95a191fd49c6930ec714e2c36fb034425
SANGJUN <sjun8932@gmail.com>
2021-11-30 09:52:43 +0900

    29. redux-thunk 이해하기

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -7,15 +7,59 @@ export const initialState = {
 }
 
 export const loginAction = (data) => {
+    return (dispatch , getState) => {
+        const state = getState();
+        setTimeout(()=>{
+            dispatch(loginRequestAction());
+        }, 2000);
+        axios.post('/api/login')
+            .then((res)=>{
+                dispatch(loginSuccessAction(res.data))
+            })
+            .catch((err)=>{
+                dispatch(loginFailureAction())
+            })
+    }
+}
+
+export const loginRequestAction = (data) => {
     return {
-        type: 'LOG_IN',
+        type: 'LOG_IN_REQUEST',
         data,
     }
 }
 
-export const logoutAction = (data) => {
+export const loginSuccessAction = (data) => {
     return {
-        type: 'LOG_OUT',
+        type: 'LOG_IN_SUCCESS',
+        data,
+    }
+}
+
+export const loginFailureAction = (data) => {
+    return {
+        type: 'LOG_IN_FAILURE',
+        data,
+    }
+}
+
+export const logoutRequestAction = (data) => {
+    return {
+        type: 'LOG_OUT_REQUEST',
+        data,
+    }
+}
+
+export const logoutSuccessAction = (data) => {
+    return {
+        type: 'LOG_OUT_SUCCESS',
+        data,
+    }
+}
+
+export const logoutRequestFailure = (data) => {
+    return {
+        type: 'LOG_OUT_FAILURE',
         data,
     }
 }
--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,11 +1,17 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, createStore, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from 'redux-thunk';
 import reducer from '../reducers/index';
+
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+    console.log(action);
+    return next(action);
+}
 
 function configureStore(){
 
-    const middlewares = [];
+    const middlewares = [thunkMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares)) : composeWithDevTools(applyMiddleware(...middlewares));
     const store = createStore(reducer, enhancer);
