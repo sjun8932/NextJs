31a626b7ef3dd04af9c86fa3ccc856281edffa91
SANGJUN <sjun8932@gmail.com>
2021-12-01 13:35:09 +0900

    31. saga 이펙트 알아보기

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,3 +1,79 @@
-export default function* rootSaga() {
+import { all , fork , take, call, put } from 'redux-saga/effects';
+import axios from 'axios';
 
+function logInAPI(data){
+    return axios.post('/api/login',data)
+}
+
+function* logIn(action) {
+    try{
+        const result = yield call(logInAPI, action.data);
+        yield put({
+            type: 'LOG_IN_SUCCESS',
+            data: result.data
+        });
+    } catch(err){
+        yield put({
+            type: 'LOG_IN_FAILURE',
+            data: err.response.data,
+        });
+    }
+}
+
+function logOutAPI(){
+    return axios.post('/api/logout')
+}
+
+function* logOut() {
+    try{
+        const result = yield call(logOutAPI);
+        yield put({
+            type: 'LOG_OUT_SUCCESS',
+            data: result.data
+        });
+    } catch(err){
+        yield put({
+            type: 'LOG_OUT_FAILURE',
+            data: err.response.data,
+        });
+    }
+}
+
+function addPostAPI(data){
+    return axios.post('/api/post',data)
+}
+
+function* addPost(action) {
+    try{
+        const result = yield call(addPostAPI, action.data);
+        yield put({
+            type: 'ADD_POST_SUCCESS',
+            data: result.data
+        });
+    } catch(err){
+        yield put({
+            type: 'ADD_POST_FAILURE',
+            data: err.response.data,
+        });
+    }
+}
+
+function* watchLogIn() {
+    yield take('LOG_IN_REQUEST',logIn)
+}
+
+function* watchLogOut() {
+    yield take('LOG_OUT_REQUEST',logOut)
+}
+
+function* watchAddPost() {
+    yield take('ADD_POST_REQUEST',addPost)
+}
+
+export default function* rootSaga() {
+    yield all([
+        fork(watchLogIn),
+        fork(watchLogOut),
+        fork(watchAddPost),
+    ]);
 }
