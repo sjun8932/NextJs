0b5de88d388172f9539e90f656d73b0d8eae1889
SANGJUN <sjun8932@gmail.com>
2021-12-02 17:31:47 +0900

    32. take, take 시리즈, throttle 알아보기

--- prepare/front/components/PostForm.js
+++ prepare/front/components/PostForm.js
@@ -22,11 +22,16 @@ const PostForm = () => {
 
     return (
         <Form style = {{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
-            <Input.TextArea value = {text} onChange = {onChangeText} maxLength={140} placeholder = "어떤 신기한 일이 있었나요" />
+            <Input.TextArea
+                value = {text}
+                onChange = {onChangeText}
+                maxLength={140}
+                placeholder = "어떤 신기한 일이 있었나요"
+            />
             <div>
                 <input type="file" multiple hidden ref={imageInput}/>
                 <Button onClick={onClickImageUpload}>이미지 업로드</Button>
-                <Button type="primary" stye={{ float: 'right'}} htmlType="submit">짹짹</Button>
+                <Button type="primary" style={{ float: 'right'}} htmlType="submit">짹짹</Button>
             </div>
             <div>
                 {imagePaths.map((v) => (
--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,4 +1,4 @@
-import { all , fork , take, call, put } from 'redux-saga/effects';
+import { all , fork , call, put, takeLatest, delay } from 'redux-saga/effects';
 import axios from 'axios';
 
 function logInAPI(data){
@@ -7,7 +7,8 @@ function logInAPI(data){
 
 function* logIn(action) {
     try{
-        const result = yield call(logInAPI, action.data);
+        /*const result = yield call(logInAPI, action.data);*/
+        yield delay(1000);
         yield put({
             type: 'LOG_IN_SUCCESS',
             data: result.data
@@ -59,15 +60,15 @@ function* addPost(action) {
 }
 
 function* watchLogIn() {
-    yield take('LOG_IN_REQUEST',logIn)
+    yield takeLatest('LOG_IN_REQUEST',logIn)
 }
 
 function* watchLogOut() {
-    yield take('LOG_OUT_REQUEST',logOut)
+    yield takeLatest('LOG_OUT_REQUEST',logOut)
 }
 
 function* watchAddPost() {
-    yield take('ADD_POST_REQUEST',addPost)
+    yield takeLatest('ADD_POST_REQUEST',addPost);
 }
 
 export default function* rootSaga() {
