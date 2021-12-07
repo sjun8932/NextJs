9aad018d56a943c132ea68e44ff76a9ac9ff68b3
SANGJUN <sjun8932@gmail.com>
2021-12-07 17:45:02 +0900

    39. faker로 실감나는 더미데이터 만들기

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppLayout from "../components/AppLayout";
 import Head from 'next/head'
 import NicknameEditForm from "../components/NicknameEditForm";
 import FollowingList from '../components/FollowingList';
 import FollowList from '../components/FollowList';
 import {useSelector} from "react-redux";
+import Router from 'next/router';
 
 const Profile = () => {
 
     const {me} = useSelector((state)=>state.user);
+
+    useEffect(()=>{
+        if(!( me&&me.id )){
+            Router.push('/');
+        }
+    },[ me && me.id ]);
+
+    if(!me){
+        return null;
+    }
 
     return(
         <>
