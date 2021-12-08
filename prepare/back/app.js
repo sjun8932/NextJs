1df3b4e4a271cf8dad64b05d038a1d8be8d5c4be
SANGJUN <sjun8932@gmail.com>
2021-12-08 15:54:52 +0900

    43. 익스프레스로 라우팅하기

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,26 +1,27 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method);
-    if (req.method === 'GET') {
-        if (req.url === '/api/posts'){
+const express = require('express');
 
-        }
-    } else if (req.method === 'POST') {
-        if (req.url === '/api/post'){
+const app = express();
 
-        }
-    } else if (req.method === 'DELETE'){
-        if (req.url === '/api/post'){
-
-        }
-    }
-    res.writeHead(200,{'Content-Type':'text/script; charset=UTF-8'});
-    res.write('<h1>안녕</h1>');
-    res.write('<h2>오늘은 좀 잘 풀리길 바래</h2>');
-    res.write('<h3>점심을 좀 많이 먹었더니</h3>');
-    res.write('<h4>배가 부르네</h4>');
-    res.end('<h5>한숨 때려야 겠다.</h5>');
+app.get('/api/posts', (req,res) => {
+    res.json([
+        {id: 1 , contents: 'happy'},
+        {id: 2 , contents: 'love'},
+        {id: 3 , contents: 'truth'},
+    ])
 });
-server.listen(3099,()=>{
+
+app.get('/', (req,res)=>{
+    res.send('hello express');
+});
+
+// app.get => 가져오다
+// app.post => 생성하다
+// app.put => 전체 수정
+// app.delete => 제거
+// app.patch => 부분 수정
+// app.options => 찔러보기
+// app.head => 헤더만 가져오기
+
+app.listen(3099,()=>{
     console.log('서버 실행 중');
 });
